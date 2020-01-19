a43cf7a65cbf8317bf87f44624bab34d1b1ccd59
Lucas Duete <lucasduete@pop-os.localdomain>
2020-01-19 17:11:29 -0300

    Finalizada aula 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
-const routes = require(`./routes`)
+const cors = require(`cors`);
+const routes = require(`./routes`);
 
 const app = express();
 mongoose.connect(`mongodb+srv://lucasduete:hGRsYVN01hNu5LSa@cluster0-4cymv.mongodb.net/semanaomnistack10?retryWrites=true&w=majority`, {
@@ -8,6 +9,7 @@ mongoose.connect(`mongodb+srv://lucasduete:hGRsYVN01hNu5LSa@cluster0-4cymv.mongo
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
